49e44f2e50e9cbf3585597a1367145e8ff9d9713
Leonardo Vinicius Wanderley Jatobá da Silva <lvwjs@ic.ufal.br>
2022-11-07 18:03:24 -0300

    Add files via upload

---
+++ p2-parte2/src/model/Aluno.java
@@ -0,0 +1,44 @@
+package model;
+import view.Payments;
+
+import java.util.*;
+
+public class Aluno extends User implements Payments {
+    public int matricula;
+    public Aluno(double salario, String name, String email, String password, int matricula){
+            super(salario, name, email, password);
+            this.matricula = matricula;
+        }
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+    // Uso de Overriding
+    @Override
+    public String toString(){
+        System.out.println(super.toString());
+        return  "numero de matrícula:" + this.matricula;
+    }
+
+    // Uso de abstract class
+    @Override
+    public void alterar(){
+            System.out.print("Digite o que voce quer que seja  alterado:");
+            Scanner s = new Scanner(System.in);
+            System.out.println("\n1-nome\n2-email\n3-senha\n4-n° de matricula\n");
+            int num = s.nextInt();
+            if(num==1){System.out.println("Digite seu novo nome: ");String v = s.next();setName(v);}
+            else if(num==2){System.out.println("Digite seu novo email: ");String v = s.next();setEmail(v);}
+            else if(num==3){System.out.println("Digite sua nova senha: ");String v = s.next();setPassword(v);}
+            else if(num==4){System.out.println("Digite seu novo n° de matricula: ");int v = s.nextInt();setMatricula(v);}
+            else{System.out.print("Erro! numero digitado errado.");}
+        }
+    @Override
+    public void recebe_salario(double dinheiro) {
+        System.out.printf("Salario caiu na conta. R$:%.4f\n", dinheiro);
+    }
+
+    @Override
+    public void recebe_salario(double dinheiro, String name) {
+        System.out.printf("Efetuando pagamento de R$:%.4f na conta do aluno %s\n", dinheiro, name);
+    }
+}
