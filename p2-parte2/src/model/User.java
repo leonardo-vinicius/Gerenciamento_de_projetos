b298515f188f27318e1d8c697e6635d4394b7338
Leonardo Vinicius Wanderley Jatobá da Silva <lvwjs@ic.ufal.br>
2022-11-07 18:13:20 -0300

    Update User.java

--- p2-parte2/src/model/User.java
+++ p2-parte2/src/model/User.java
@@ -5,7 +5,6 @@ public abstract class User {
     public String name;
     public String email;
     public String password;
-
     public User(double salario, String name, String email, String password) {
         this.setSalario(salario);
         this.name = name;
@@ -21,11 +20,9 @@ public abstract class User {
     public void setName(String name) {
         this.name = name;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
@@ -35,7 +32,6 @@ public abstract class User {
     public void setSalario(double salario) {
         this.salario = salario;
     }
-
     public abstract void alterar();
 
 }
